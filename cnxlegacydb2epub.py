410a64a983e6fa1b81c45df810e4fe98cf0c4528
Michael Mulich <michael.mulich@gmail.com>
2014-02-24 00:11:05 -0500

    Uses cnx-archive based Reference resolver, which is incomplete for this content.

--- cnxlegacydb2epub.py
+++ cnxlegacydb2epub.py
@@ -159,16 +159,16 @@ def extract_content(id, version, db_cursor):
     raise StopIteration()
 
 
-def extract_resources(idents, db_cursor):
+def extract_resources(content, db_cursor):
     """Returns a list of resource files given a list of
     module_idents (the internal primary key for modules).
     """
     # Extract resource files.
-    args = {'idents': idents}
+    args = {'module_ident': content['_ident']}
     db_cursor.execute(SQL_GET_FILES, args)
     # List of (md5, mediatype, filename, buff,)
-    resources = db_cursor.fetchall()
-
+    yield from db_cursor.fetchall()
+    raise StopIteration()
 
 # HTML namespace mapping
 HTML_NAMESPACE = "http://www.w3.org/1999/xhtml"
@@ -213,7 +213,7 @@ def fix_content(content):
                                     for elm in module_body])
 
 
-def render_to_html(content):
+def render_to_html(content, db_connection):
     """Render the given content to HTML."""
     info = content.copy()
     if content['_type'] == COLLECTION_TYPE:
@@ -227,7 +227,13 @@ def render_to_html(content):
         'head': head_template.render(**info),
         'body': body_template.render(**info),
         }
-    return html_template.render(**html_blocks)
+    html = html_template.render(**html_blocks)
+    # Fix references.
+    if content['_type'] == MODULE_TYPE:
+        html, bad_refs = fix_reference_urls(db_connection,
+                                            content['_ident'],
+                                            html)
+    return html
 
 
 def main(argv=None):
@@ -241,6 +247,7 @@ def main(argv=None):
     else:
         filepath = args['<file>']
     epub = zipfile.ZipFile(filepath, 'w')
+    epub.writestr('mimetype', 'application/epub+zip')
 
     try:
         id, version = args['<ident-hash>'].split('@')
@@ -251,10 +258,11 @@ def main(argv=None):
     with psycopg2.connect(psycopg2_db_conn_str) as db_conn:
         with db_conn.cursor() as cursor:
             for content in extract_content(id, version, cursor):
+                # Write the content to the EPUB.
                 filename = "{}@{}.html".format(content['id'],
                                                content['version'])
                 arc_filepath = os.path.join('contents', filename)
-                epub.writestr(arc_filepath, render_to_html(content))
+                epub.writestr(arc_filepath, render_to_html(content, db_conn))
 
                 MSG = "{} - {}"
                 print(MSG.format(arc_filepath, content['title']))
@@ -270,6 +278,295 @@ def main(argv=None):
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
 
+# ######################## #
+#   Reference Resolution   #
+# ######################## #
+
+
+class BaseReferenceException(Exception):
+    """Not for direct use, but used to subclass other exceptions."""
+
+    def __init__(self, message, document_ident, reference):
+        self.document_ident = document_ident
+        self.reference = reference
+        self.args = (message, document_ident, reference,)
+
+    def __str__(self):
+        return "{}: document={}, reference={}".format(*self.args)
+
+    def __repr__(self):
+        cls_name = self.__class__.__name__
+        return "<{} - {}>".format(cls_name, str(self))
+
+
+class ReferenceNotFound(BaseReferenceException):
+    """Used when a reference to a resource can't be found."""
+
+
+class InvalidReference(BaseReferenceException):
+    """Used when a reference by all known accounts appears to be invalid."""
+
+    def __init__(self, document_ident, reference):
+        msg = "Invalid reference value"
+        super(InvalidReference, self).__init__(msg, document_ident, reference)
+
+
+PATH_REFERENCE_REGEX = re.compile(
+    r'^(/?(content/)? *(?P<module>(m|col)\d{4,5})([/@](?P<version>([.\d]+|latest)))?)?/?(?P<resource>[^#][ -_.@\w\d]+)?(?P<fragment>#?.*)?$',
+    re.IGNORECASE)
+MODULE_REFERENCE = 'module-reference'
+RESOURCE_REFERENCE = 'resource-reference'
+
+
+def parse_reference(ref):
+    """Parse the reference to a reference type and type specific value.
+    A module-reference value contains the id, version and fragment.
+    A resource-reference value resource filename.
+    """
+    match = PATH_REFERENCE_REGEX.match(ref)
+    try:
+        # Dictionary keyed by named groups, None values for no match
+        matches = match.groupdict()
+    except: # None type
+        raise ValueError("Unable to parse reference with value '{}'" \
+                         .format(ref))
+
+    version = matches['version']
+    if version == 'latest':
+        version = None
+
+    # We've got a match, but what kind of thing is it.
+    if matches['resource']:
+        type = RESOURCE_REFERENCE
+        value = (matches['resource'].strip(), matches['module'], version)
+    elif matches['module']:
+        type = MODULE_REFERENCE
+        value = (matches['module'], version, matches['fragment'])
+    else:
+        type = None
+        value = ()
+    return type, value
+
+
+SQL_MODULE_UUID_N_VERSION_BY_ID_STATEMENT = """\
+SELECT uuid, concat_ws('.', major_version, minor_version) FROM latest_modules
+WHERE moduleid = %s
+"""
+SQL_MODULE_UUID_N_VERSION_BY_ID_AND_VERSION_STATEMENT = """\
+SELECT uuid, concat_ws('.', major_version, minor_version) FROM modules
+WHERE moduleid = %s and version = %s
+"""
+SQL_DOCUMENT_IDENT_BY_ID_N_VERSION = """\
+SELECT module_ident FROM modules
+WHERE moduleid = %s and version = %s
+"""
+SQL_LATEST_DOCUMENT_IDENT_BY_ID = """\
+SELECT module_ident FROM latest_modules
+WHERE moduleid = %s
+"""
+SQL_RESOURCE_INFO_STATEMENT = """\
+SELECT row_to_json(row) FROM (
+SELECT fileid as id, md5 as hash, filename as filename, mimetype as mediatype
+FROM module_files natural join files
+WHERE fileid = (
+  SELECT fileid FROM module_files
+  WHERE module_ident = %s AND filename = %s
+  )
+) row;
+"""
+
+
+class ReferenceResolver:
+
+    def __init__(self, db_connection, document_ident, html):
+        self.db_connection = db_connection
+        self.document_ident = document_ident
+        self.document = etree.fromstring(html)
+        self.namespaces = self.document.nsmap.copy()
+        if None in self.namespaces:
+            # The xpath method on an Element doesn't like 'None' namespaces.
+            self.namespaces.pop(None)
+            # The None namespace is assumed to be xhtml, redeclared below.
+        self.namespaces['html'] = 'http://www.w3.org/1999/xhtml'
+
+    def __call__(self):
+        messages = []
+        messages.extend(self.fix_media_references())
+        messages.extend(self.fix_anchor_references())
+        messages = [str(e) for e in messages]
+        return etree.tostring(self.document), messages
+
+    @classmethod
+    def fix_reference_urls(cls, db_connection, document_ident, html):
+        resolver = cls(db_connection, document_ident, html)
+        return resolver()
+
+    def get_uuid_n_version(self, module_id, version=None):
+        with self.db_connection.cursor() as cursor:
+            if version:
+                cursor.execute(
+                    SQL_MODULE_UUID_N_VERSION_BY_ID_AND_VERSION_STATEMENT,
+                    (module_id, version))
+            else:
+                cursor.execute(
+                    SQL_MODULE_UUID_N_VERSION_BY_ID_STATEMENT,
+                    (module_id,))
+            try:
+                uuid, version = cursor.fetchone()
+            except (TypeError, ValueError): # None or unpack problem
+                uuid, version = (None, None,)
+        return uuid, version
+
+    def get_resource_info(self, filename, document_id=None, version=None):
+        document_ident = self.document_ident
+        with self.db_connection.cursor() as cursor:
+            if document_id:
+                if version:
+                    cursor.execute(
+                        SQL_DOCUMENT_IDENT_BY_ID_N_VERSION,
+                        (document_id, version,))
+                    try:
+                        document_ident = cursor.fetchone()[0]
+                    except TypeError:
+                        raise ReferenceNotFound(
+                            "Missing resource with filename '{}', " \
+                            "moduleid {} version {}." \
+                            .format(filename, document_id, version),
+                            document_ident, filename)
+                else:
+                    cursor.execute(
+                        SQL_LATEST_DOCUMENT_IDENT_BY_ID,
+                        [document_id])
+                    try:
+                        document_ident = cursor.fetchone()[0]
+                    except TypeError:
+                        raise ReferenceNotFound(
+                            "Missing resource with filename '{}', " \
+                            "moduleid {} version {}." \
+                            .format(filename, document_id, version),
+                            document_ident, filename)
+
+            cursor.execute(SQL_RESOURCE_INFO_STATEMENT,
+                           (document_ident, filename,))
+            try:
+                info = cursor.fetchone()[0]
+            except TypeError:
+                raise ReferenceNotFound(
+                    "Missing resource with filename '{}', "
+                    "moduleid {} version {}." \
+                    .format(filename, document_id, version),
+                    document_ident, filename)
+            else:
+                if isinstance(info, basestring):
+                    info = json.loads(info)
+                return info
+
+    def apply_xpath(self, xpath):
+        """Apply an XPath statement to the document."""
+        return self.document.xpath(xpath, namespaces=self.namespaces)
+
+    def _should_ignore_reference(self, ref):
+        """Given an href string, determine if it should be ignored.
+For example, external links and mailto references should be ignored.
+"""
+        ref = ref.strip()
+        should_ignore = not ref \
+                        or ref.startswith('#') \
+                        or ref.startswith('http') \
+                        or ref.startswith('mailto') \
+                        or ref.startswith('file') \
+                        or ref.startswith('/help') \
+                        or ref.startswith('ftp') \
+                        or ref.startswith('javascript:')
+        return should_ignore
+
+    def fix_media_references(self):
+        """Fix references to interal resources."""
+        # Catch the invalid, unparsable, etc. references.
+        bad_references = []
+
+        media_xpath = {
+                '//html:img': 'src',
+                '//html:audio': 'src',
+                '//html:video': 'src',
+                '//html:object': 'data',
+                '//html:object/html:embed': 'src',
+                '//html:source': 'src',
+                '//html:span': 'data-src',
+                }
+
+        for xpath, attr in media_xpath.items():
+            for elem in self.apply_xpath(xpath):
+                filename = elem.get(attr)
+                if not filename or self._should_ignore_reference(filename):
+                    continue
+
+                try:
+                    ref_type, payload = parse_reference(filename)
+                    filename, module_id, version = payload
+                except ValueError:
+                    exc = InvalidReference(self.document_ident, filename)
+                    bad_references.append(exc)
+                    continue
+
+                try:
+                    info = self.get_resource_info(filename, module_id, version)
+                except ReferenceNotFound as exc:
+                    bad_references.append(exc)
+                else:
+                    elem.set(attr, '../resources/{}'.format(info['hash'],))
+                    elem.set('data-filename', info['filename'])
+                    elem.set('data-mediatype', info['mediatype'])
+        return bad_references
+
+    def fix_anchor_references(self):
+        """Fix references to internal documents and resources."""
+        # Catch the invalid, unparsable, etc. references.
+        bad_references = []
+
+        for anchor in self.apply_xpath('//html:a'):
+            ref = anchor.get('href')
+            if not ref or self._should_ignore_reference(ref):
+                continue
+
+            try:
+                ref_type, payload = parse_reference(ref)
+            except ValueError:
+                exc = InvalidReference(self.document_ident, ref)
+                bad_references.append(exc)
+                continue
+
+            if ref_type == MODULE_REFERENCE:
+                module_id, version, url_frag = payload
+                uuid, version = self.get_uuid_n_version(module_id, version)
+                if uuid is None:
+                    bad_references.append(
+                        ReferenceNotFound("Unable to find a reference to "
+                                          "'{}' at version '{}'." \
+                                              .format(module_id, version),
+                                          self.document_ident, ref))
+                else:
+                    url_frag = url_frag and url_frag or ''
+                    path = '{}@{}.html{}'.format(uuid, version,
+                                                           url_frag)
+                    anchor.set('href', path)
+            elif ref_type == RESOURCE_REFERENCE:
+                try:
+                    filename, module_id, version = payload
+                    info = self.get_resource_info(filename, module_id, version)
+                except ReferenceNotFound as exc:
+                    bad_references.append(exc)
+                else:
+                    anchor.set('href', '../resources/{}'.format(info['hash']))
+                    anchor.set('data-filename', info['filename'])
+                    anchor.set('data-mediatype', info['mediatype'])
+            else:
+                exc = InvalidReference(self.document_ident, ref)
+                bad_references.append(exc)
+
+        return bad_references
+
+fix_reference_urls = ReferenceResolver.fix_reference_urls
 
 
 # From here down be Dragons!
@@ -294,7 +591,7 @@ HTML_HEAD_TEMPLATE = """\
  <title>{{ title }}</title>
  <meta name="dc:license" content="{{ license.name }}" />
  <link rel="lrmi:useRightsURL" href="{{ license.url }}"/>
- <meta itemprop="inLanguage" content="{{ language }}">
+ <meta itemprop="inLanguage" content="{{ language }}" />
  <meta itemprop="accessibilityFeature" content="MathML" />
  <meta itemprop="accessibilityFeature" content="alternativeText" />
  {% for keyword in keywords -%}
@@ -370,6 +667,7 @@ HTML_BODY_TEMPLATE = """\
              itemprop="url"
              data-type="cnx-id"
              >{{ submitter.fullname }}</a>
+      </span>
     </div>
 
     {% if basedOn is defined %}
@@ -428,7 +726,7 @@ HTML_BODY_TEMPLATE = """\
     </div>
     {% endif %}
 
-   <div class="description"
+    <div class="description"
         itemprop="description"
         data-type="description"
         >
@@ -566,5 +864,8 @@ where module_ident = %(module_ident)s
 """
 
 SQL_GET_FILES = """\
-
+SELECT md5, mimetype, file
+FROM module_files NATURAL JOIN files
+WHERE module_ident = %(module_ident)s
+      AND filename != any(['collection.xml', 'index.cnxml', 'index.cnxml.html'])
 """
